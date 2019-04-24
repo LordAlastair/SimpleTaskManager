ec9bfee1665cbf4d2430f2fa9529c2dbc1eef032
Harlan Gomes <harlangomesnascimento@outlook.com>
2019-04-23 22:17:16 -0300

    Done field added to Task

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-   protected $fillable = ['name' | 'price' | 'deadline' | 'presentation_order' ];
+   protected $fillable = ['name' | 'price' | 'deadline' | 'presentation_order' | 'done'];
 }
