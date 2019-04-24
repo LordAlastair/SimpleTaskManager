539a359d3077c60f972042dd1fb9d7af01f4dcb0
Harlan Gomes <harlangomesnascimento@outlook.com>
2019-04-23 22:27:53 -0300

    Seed Data for tasks

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Task;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        Task::truncate();
+        $faker = \Faker\Factory::create();
+
+        for($i = 0; $i < 20; $i++){
+            Task::create([
+                'name' =>  'Awesome job task ' . $i,
+                'price' =>  $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 8),
+                'deadline' => $faker->dateTimeBetween($startDate = '-1 days', $endDate = '+10 days', $timezone = 'UTC -3'),
+                'done' => $faker->boolean($chanceOfGettingTrue = 75),
+                'presentation_order' => $i
+            ]);
+        }
     }
 }
